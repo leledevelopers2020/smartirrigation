eca9ac5ce2f2ab93d0041f2c7c20d915d6a20baa
NarsingRao13 <narsingrao.k@techouts.com>
2021-09-19 01:32:29 +0530

    Screen 1 and Screen 2.1 update

---
+++ app/src/main/java/com/leledevelopers/smartirrigation/utils/ProjectUitls.java
@@ -0,0 +1,12 @@
+package com.leledevelopers.smartirrigation.utils;
+
+/**
+ * @author Narsing Rao.k
+ * This class contains all the constants of this project
+ */
+public class ProjectUitls {
+    public static final String EXTRA_SMS_SENDER = "extra_sms_sender";
+    public static final String EXTRA_SMS_MESSAGE = "extra_sms_message";
+    public static final int SMS_REQUEST_CODE = 101;
+    public static final int PICK_CONTACT = 102;
+}
