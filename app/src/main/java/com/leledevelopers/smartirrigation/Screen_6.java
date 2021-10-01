4969fd641f660e00f8cfa6a47353049a4b70d987
Shivam <leledevelopers2020@gmail.com>
2021-10-02 00:44:41 +0530

    initviews

--- app/src/main/java/com/leledevelopers/smartirrigation/Screen_6.java
+++ app/src/main/java/com/leledevelopers/smartirrigation/Screen_6.java
@@ -3,19 +3,37 @@ package com.leledevelopers.smartirrigation;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.TextView;
 
 public class Screen_6 extends AppCompatActivity {
 
     private Spinner spinner;
+    EditText wetPeriod,injectPeriod,noOfIterations;
+    private Button enableFieldFertigation,disableFieldFertigation;
+    private ArrayAdapter<CharSequence> adapter;
+    private TextView status7;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_screen6);
+        adapter= ArrayAdapter.createFromResource(getApplicationContext(),R.array.selctFieldNoArray,android.R.layout.simple_spinner_dropdown_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
     }
     private void initViews()
     {
         spinner=(Spinner) findViewById(R.id.fieldNoSpinner6);
+        wetPeriod=findViewById(R.id.wetPeriod);
+        injectPeriod=findViewById(R.id.injectPeriod);
+        noOfIterations=findViewById(R.id.noOfIterations);
+        enableFieldFertigation=findViewById(R.id.enableFieldFertigation6);
+        disableFieldFertigation=findViewById(R.id.disableFieldFertigation6);
+        status7=findViewById(R.id.screen_6_status);
+
     }
 }
--- app/src/main/java/com/leledevelopers/smartirrigation/Screen_9.java
+++ app/src/main/java/com/leledevelopers/smartirrigation/Screen_9.java
@@ -3,19 +3,34 @@ package com.leledevelopers.smartirrigation;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.Spinner;
+import android.widget.TextView;
 
 public class Screen_9 extends AppCompatActivity {
 
     private Spinner spinner;
+    private ArrayAdapter<CharSequence> adapter;
+    private Button setSystemTime,getSystemTime,updatePassword,setMotorloadCutoff,save;
+    private TextView status9;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_screen9);
+        adapter= ArrayAdapter.createFromResource(getApplicationContext(),R.array.selctFieldNoArray,android.R.layout.simple_spinner_dropdown_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
     }
     private void initViews()
     {
         spinner=(Spinner) findViewById(R.id.language_spinner);
+        setSystemTime=findViewById(R.id.setSystemTime);
+        getSystemTime=findViewById(R.id.getSystemTime);
+        updatePassword=findViewById(R.id.updatePassword);
+        setMotorloadCutoff=findViewById(R.id.setMotorloadCutoff);
+        status9=findViewById(R.id.screen_9_status);
+        save=findViewById(R.id.save);
     }
 }
