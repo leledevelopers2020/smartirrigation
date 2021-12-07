17911514bd1d1cc912b65513287909062dfd2be3
Shivam <leledevelopers2020@gmail.com>
2021-12-07 22:45:51 +0530

    status settings

--- app/src/main/java/com/leledevelopers/smartirrigation/registration/Screen_2_1.java
+++ app/src/main/java/com/leledevelopers/smartirrigation/registration/Screen_2_1.java
@@ -59,8 +59,9 @@ public class Screen_2_1 extends SmsServices {
     private boolean isSetClicked = false, isGSMSelected = false, isPasswordSaved = false;
     private CheckBox checkbox1, checkbox2;
     private String smsData;
-    Intent intent;
-    Boolean extra=false;
+    private Intent intent;
+    private Bundle bundle;
+    private Boolean extra=false;
 
 
     @Override
@@ -68,10 +69,13 @@ public class Screen_2_1 extends SmsServices {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_screen21);
         System.out.println("create method 2.1");
+
         try
         {
             intent=getIntent();
-           extra= intent.getParcelableExtra("Settings");
+            bundle=intent.getExtras();
+            if(bundle!=null)
+                extra= bundle.getBoolean("Settings");
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -507,16 +511,13 @@ public class Screen_2_1 extends SmsServices {
                 startActivity(new Intent(Screen_2_1.this, Screen_4.class));
                 finish();
             } else {
+                Log.d("screen 2.1","hello screen 2.1");
+
                 startActivity(new Intent(Screen_2_1.this, Screen_1.class));
                 finish();
             }
         } catch (Exception e) {
             e.printStackTrace();
-        }
-        finally {
-            Log.d("TAG","finally block :"+extra);
-            startActivity(new Intent(Screen_2_1.this, Screen_1.class));
-            finish();
         }
 
     }
