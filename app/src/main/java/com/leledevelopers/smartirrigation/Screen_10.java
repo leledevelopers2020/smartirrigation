fb23c3152104776b04dc882d4b780e6d6ba62b99
Shivam <leledevelopers2020@gmail.com>
2022-01-08 23:03:11 +0530

    05/01/2022 observation changes

--- app/src/main/java/com/leledevelopers/smartirrigation/Screen_10.java
+++ app/src/main/java/com/leledevelopers/smartirrigation/Screen_10.java
@@ -225,9 +225,9 @@ public class Screen_10 extends SmsServices {
                         @Override
                         public void run() {
                             Intent intent=(new Intent(Screen_10.this, MainActivity_GSM.class));
-                            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP|Intent.FLAG_ACTIVITY_NEW_TASK);
+                            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK|Intent.FLAG_ACTIVITY_NEW_TASK);
                             startActivity(intent);
-                            finish();
+                           // finish();
                         }
                     }, 3000);
                 }
